928931766404862e02680df25489903d89f6d96b
Quarantinoo <vighnesh.chauhan@yahoo.com>
2025-08-04 11:27:08 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("📈 MarketAlphaElite")
+                .font(.largeTitle)
+                .padding()
+            Text("Live Trades • Probable Entries • Alerts")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
